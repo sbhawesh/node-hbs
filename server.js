803a28f2c11ef95336ed194c8051d9f7bd2703eb
Bhawesh Singh <sbhaweshsingh@gmail.com>
2018-05-27 16:53:12 +0530

    setup start scrpit and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 app.set('view engine','hbs');
 hbs.registerPartials(__dirname + '/views/partials');
@@ -45,6 +46,6 @@ app.get('/bad',(req,res) =>{
 	res.send('error in request');
 });
 
-app.listen(3000,() => {
+app.listen(port,() => {
        console.log("server started");
 	});
